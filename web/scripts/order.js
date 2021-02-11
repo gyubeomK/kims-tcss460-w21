efb99039695f42af1f5829d2816869ee8116ffc2
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-02-10 18:58:51 -0800

    implementing putting order data into the database

---
+++ web/scripts/order.js
@@ -0,0 +1,13 @@
+function order() {
+    console.log("order triggered")
+    let sizeInput = document.querySelector('input[name = "size"]:checked').value;
+    console.log(sizeInput)
+
+
+
+
+
+
+
+    // 인풋 전부 다 정리 후, sql 데타 베이스에 넣기
+}
