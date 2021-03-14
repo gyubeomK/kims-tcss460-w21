99fda022bef10441d1f61d4de7dc06ec9248c0f4
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-14 12:26:19 -0700

    creating cookie implemented

--- web/scripts/login_reg.js
+++ web/scripts/login_reg.js
@@ -40,7 +40,7 @@ async function sign_in(email, password) {
         }
     })
 
-    console.log("Response: " + response.ok)
+    console.log("Response: " + response.ok) 
 
     if (response.ok) { // if HTTP-status is 200-299
         // get the response body (the method explained below)
