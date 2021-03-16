03b1b92bcb1f449f4b152a8a0676a003d26f3b03
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-16 11:18:37 -0700

    implementing removing from cart

--- routes/favOrder.js
+++ routes/favOrder.js
@@ -163,4 +163,41 @@ router.post('/', (request, response) => {
     }
 })
 
+router.delete("/", (request, response) => { 
+    console.log("request.body.favID" + request.body.favID)
+    if (request.body.favID != null) {
+        
+        const theQuery = `DELETE FROM FavPizza WHERE favID = $1 RETURNING *`    
+        const values = [request.body.favID]
+
+        pool.query(theQuery, values)
+            .then(result => {
+                
+                if (result.rowCount == 1) {
+                    
+                    response.send({
+                        success: true,
+                        message: "Deleted: " + result.rows[0].name
+                    })
+                } else {
+                    
+                    response.status(404).send({
+                        message: "Name not found"
+                    })
+                }
+            })
+            .catch(err => {
+                //log the error
+                // console.log(err)
+                response.status(400).send({
+                    message: err.detail
+                })
+            }) 
+    } else {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    }     
+})
+
 module.exports = router
