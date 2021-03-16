f465630df00bc91d5483b524ee688e9006a3bf3b
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-16 09:11:02 -0700

    implementing cart for authenticated users

--- web_restricted/scripts/cart.js
+++ web_restricted/scripts/cart.js
@@ -72,10 +72,10 @@ async function getCartItem() {
                 // $("#cartSpecific").append($("<li style='font-size:20px'>").text(output))
                 // $("#cartSpecific").append($("<li style='font-size:14px'>").text(price))
                 
-                $(".cartSpecific").append($("<div class='cart-items'>").append($("<div class='cart-item cart-column;>")))
+                $(".cartSpecific").append($("<div class='cart-items'>").append($("<div class='cart-item cart-column;>")).append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":")).append($("<li style='font-size:14px'>").text(size)))
                                 
                                 //.append($("<img class='cart-item-image' src='../images/seafood-pizza.jpg' width='100' height='100'>))
-                                .append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":")).append($("<li style='font-size:14px'>").text(size)).append($("<li style='font-size:14px'>").text(crust)).append($("<li style='font-size:14px'>").text(cheese)).append($("<li style='font-size:14px'>").text(sauce)).append($("<li style='font-size:14px'>").text(toppings)).append($("<li style='font-size:14px'>").text(sToppings))
+                                .append($("<li style='font-size:14px'>").text(crust)).append($("<li style='font-size:14px'>").text(cheese)).append($("<li style='font-size:14px'>").text(sauce)).append($("<li style='font-size:14px'>").text(toppings)).append($("<li style='font-size:14px'>").text(sToppings))
                                 .append($("<span class='cart-price cart-column' style='margin-left: 8em;' id='price'>" + json.orders[i].price + "</span>"))
                                 .append($("<input class='cart-quantity-input' type='number' value='1' style='margin-left: 2em;'>"))
                                 .append($("<button class='btn btn-danger' type='button'>REMOVE</button>"))
