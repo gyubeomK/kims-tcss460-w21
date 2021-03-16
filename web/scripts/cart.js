608c0fae5d4f365cac7a8bb064ce3975e8d89c29
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-16 05:29:35 -0700

    implementing web_restricted documents

--- web/scripts/cart.js
+++ web/scripts/cart.js
@@ -9,90 +9,95 @@ let thirdIngList = []
 
 window.onload = function() {
     console.log("Triggered on reload()")
-    if (document.cookie.indexOf('authorized') == -1 ) {
-        $("#cartPanel").remove()
+    getCartItem()
+    // if (document.cookie.indexOf('authorized') == -1 ) {
+    //     $("#cartPanel").remove()
         
-        alert("You need to be signed in to use cart feature")
-    } else {
-        getCartItem()
-    }
+    //     alert("You need to be signed in to use cart feature")
+    // } else {
+        
+    // }
     
 
 }
 
 async function getCartItem() {
     console.log("getCartItem() triggered")
-
-
-    let response = await fetch("/cart",  {
-        method: 'GET',
-        headers: {
-            "Content-Type" : "application/json;charset=utf-8"
-        }
-    })
-    if (response.ok) { 
-        let json = await response.json()
-        console.log(json)
+    if(document.cookie.indexOf('authorized') == -1 ) {
+        alert("You need to be signed in to use cart feature")
+        //$("#cartPanel").remove()
+                
         
-        for(let i = 0; i < json.orders.length; i++) {
-            totalList.push(json.orders[i].total)
-            sizeList.push(json.orders[i].size)
-            crustList.push(json.orders[i].crust)
-            cheeseList.push(json.orders[i].cheese)
-            sauceList.push(json.orders[i].sauce)
-            secIngList.push(json.orders[i].secing)
-            thirdIngList.push(json.orders[i].thirding)
-            
-            
-            
-
-            // let output = "Price: " + json.orders[i].total
-            //             + ", Size: " + json.orders[i].size 
-            //             + ", Crust: " + json.orders[i].crust
-            //             + ", Cheese: " + json.orders[i].cheese
-            //             + ", Sauce: " + json.orders[i].sauce
-            //             + ", Toppings: " + json.orders[i].secing
-            //             + ", Special Toppings: " + json.orders[i].thirding
-
-            
-            let size = "Size: " + json.orders[i].size 
-            let crust = "Crust: " + json.orders[i].crust
-            let cheese = "Cheese: " + json.orders[i].cheese
-            let sauce = "Sauce: " + json.orders[i].sauce
-            let toppings = "Toppings: " + json.orders[i].secing
-            let sToppings = "Special Toppings: " + json.orders[i].thirding
-
-
-            
-            // $("#cartSpecific").append($("<li style='font-size:20px'>").text(output))
-            // $("#cartSpecific").append($("<li style='font-size:14px'>").text(price))
-            
-
-            $("#cartSpecific").append($("<li style='font-size:14px'>").text(size))
-            $("#cartSpecific").append($("<li style='font-size:14px'>").text(crust))
-            $("#cartSpecific").append($("<li style='font-size:14px'>").text(cheese))
-            $("#cartSpecific").append($("<li style='font-size:14px'>").text(sauce))
-            $("#cartSpecific").append($("<li style='font-size:14px'>").text(toppings))
-            $("#cartSpecific").append($("<li style='font-size:14px'>").text(sToppings))
-
-            document.getElementById("price").innerHTML = "$" + json.orders[i].total
-            document.getElementById("totalPrice").innerHTML = "$" + json.orders[i].total
-            
-
-            
-            
-            
-        }
-        
-
-
     } else {
-        alert("HTTP-Error: " + response.status)
-        console.log(response.status)
-        let json = await response.json()
-        console.log(json)
-    }                
+        let response = await fetch("/cart",  {
+            method: 'GET',
+            headers: {
+                "Content-Type" : "application/json;charset=utf-8"
+            }
+        })
+        if (response.ok) { 
+            let json = await response.json()
+            console.log(json)
+            
+            for(let i = 0; i < json.orders.length; i++) {
+                totalList.push(json.orders[i].total)
+                sizeList.push(json.orders[i].size)
+                crustList.push(json.orders[i].crust)
+                cheeseList.push(json.orders[i].cheese)
+                sauceList.push(json.orders[i].sauce)
+                secIngList.push(json.orders[i].secing)
+                thirdIngList.push(json.orders[i].thirding)
+                
+                
+                
 
+                // let output = "Price: " + json.orders[i].total
+                //             + ", Size: " + json.orders[i].size 
+                //             + ", Crust: " + json.orders[i].crust
+                //             + ", Cheese: " + json.orders[i].cheese
+                //             + ", Sauce: " + json.orders[i].sauce
+                //             + ", Toppings: " + json.orders[i].secing
+                //             + ", Special Toppings: " + json.orders[i].thirding
+
+                
+                let size = "Size: " + json.orders[i].size 
+                let crust = "Crust: " + json.orders[i].crust
+                let cheese = "Cheese: " + json.orders[i].cheese
+                let sauce = "Sauce: " + json.orders[i].sauce
+                let toppings = "Toppings: " + json.orders[i].secing
+                let sToppings = "Special Toppings: " + json.orders[i].thirding
+
+
+                
+                // $("#cartSpecific").append($("<li style='font-size:20px'>").text(output))
+                // $("#cartSpecific").append($("<li style='font-size:14px'>").text(price))
+                
+
+                $("#cartSpecific").append($("<li style='font-size:14px'>").text(size))
+                $("#cartSpecific").append($("<li style='font-size:14px'>").text(crust))
+                $("#cartSpecific").append($("<li style='font-size:14px'>").text(cheese))
+                $("#cartSpecific").append($("<li style='font-size:14px'>").text(sauce))
+                $("#cartSpecific").append($("<li style='font-size:14px'>").text(toppings))
+                $("#cartSpecific").append($("<li style='font-size:14px'>").text(sToppings))
+
+                document.getElementById("price").innerHTML = "$" + json.orders[i].total
+                document.getElementById("totalPrice").innerHTML = "$" + json.orders[i].total
+                
+
+                
+                
+                
+            }
+            
+
+
+        } else {
+            alert("HTTP-Error: " + response.status)
+            console.log(response.status)
+            let json = await response.json()
+            console.log(json)
+        }                
+    }
 }
 async function orderSubmit() {
     console.log("orderSubmit() triggered")
