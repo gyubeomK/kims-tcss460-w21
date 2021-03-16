52531e931bcc4ff2bdd026b79f2f88ba8c25619c
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-16 08:14:07 -0700

    implementing cart for authenticated users

--- web_restricted/scripts/cart.js
+++ web_restricted/scripts/cart.js
@@ -72,7 +72,7 @@ async function getCartItem() {
                 // $("#cartSpecific").append($("<li style='font-size:20px'>").text(output))
                 // $("#cartSpecific").append($("<li style='font-size:14px'>").text(price))
                 
-
+                $("#cartSpecific").append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":"))
                 $("#cartSpecific").append($("<li style='font-size:14px'>").text(size))
                 $("#cartSpecific").append($("<li style='font-size:14px'>").text(crust))
                 $("#cartSpecific").append($("<li style='font-size:14px'>").text(cheese))
