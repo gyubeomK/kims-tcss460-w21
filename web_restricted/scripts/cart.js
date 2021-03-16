229bd93a589c315d8894a2c7e7c012a029eb0710
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-16 08:49:31 -0700

    implementing cart for authenticated users

--- web_restricted/scripts/cart.js
+++ web_restricted/scripts/cart.js
@@ -75,18 +75,21 @@ async function getCartItem() {
                 $(".cartSpecific").append($("<div class='cart-items'>"))
                                 .append($("<div class='cart-item cart-column;>"))
                                 //.append($("<img class='cart-item-image' src='../images/seafood-pizza.jpg' width='100' height='100'>))
-                                .append($("<span class='cart-item-title'>"))
+                                .append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":")).append($("<li style='font-size:14px'>").text(size)).append($("<li style='font-size:14px'>").text(crust)).append($("<li style='font-size:14px'>").text(cheese)).append($("<li style='font-size:14px'>").text(sauce)).append($("<li style='font-size:14px'>").text(toppings))
                                 .append($("<span class='cart-price cart-column' style='margin-left: 8em;' id='price'></span>"))
-                                .append($("<input class='cart-quantity-input' type='number' value='1' style='margin-left: 20em;'>"))
+                                .append($("<input class='cart-quantity-input' type='number' value='1' style='margin-left: 2em;'>"))
                                 .append($("<button class='btn btn-danger' type='button'>REMOVE</button>"))
 
-                $(".cartSpecific").append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":"))
-                $(".cartSpecific").append($("<li style='font-size:14px'>").text(size))
-                $(".cartSpecific").append($("<li style='font-size:14px'>").text(crust))
-                $(".cartSpecific").append($("<li style='font-size:14px'>").text(cheese))
-                $(".cartSpecific").append($("<li style='font-size:14px'>").text(sauce))
-                $(".cartSpecific").append($("<li style='font-size:14px'>").text(toppings))
-                $(".cartSpecific").append($("<li style='font-size:14px'>").text(sToppings))
+                // $(".cartSpecific").append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":"))
+                // $(".cartSpecific").append($("<li style='font-size:14px'>").text(size))
+                // $(".cartSpecific").append($("<li style='font-size:14px'>").text(crust))
+                // $(".cartSpecific").append($("<li style='font-size:14px'>").text(cheese))
+                // $(".cartSpecific").append($("<li style='font-size:14px'>").text(sauce))
+                // $(".cartSpecific").append($("<li style='font-size:14px'>").text(toppings))
+                // $(".cartSpecific").append($("<li style='font-size:14px'>").text(sToppings))
+
+
+                //$(".cartSpecific").append($("<span class='cart-item-title'>").text("Pizza " + (i + 1) + ":")).append($("<li style='font-size:14px'>").text(size)).append($("<li style='font-size:14px'>").text(crust)).append($("<li style='font-size:14px'>").text(cheese)).append($("<li style='font-size:14px'>").text(sauce)).append($("<li style='font-size:14px'>").text(toppings)).append($("<li style='font-size:14px'>").text(sToppings))
 
                 //document.getElementById("price").innerHTML = "$" + json.orders[i].total
                 //document.getElementById("totalPrice").innerHTML = "$" + json.orders[i].total
