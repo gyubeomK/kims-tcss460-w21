d0107eb58a4412cdeb40802771dcd3b00d1624b2
Hyeong Suk Kim <whddkf2004@gmail.com>
2021-03-16 19:00:52 -0700

    working on changing item in Cart and Order pages

--- web_restricted/scripts/cart.js
+++ web_restricted/scripts/cart.js
@@ -78,7 +78,7 @@ async function getCartItem() {
                                             .append($("<span class='cart-price cart-column' style='margin-left: 11em;display: inline-grid;' id='price'>$" + json.orders[i].total + "</span>"))
                                             //.append($("<input class='cart-quantity-input' type='number' value='1' style='margin-left: 2em;'>"))
                                             //.append($("<button class='btn btn-danger' data-toggle='modal' data-target='#myCartModal' onclick='editCartItem("+json.orders[i].cartid+")' type='button'>Edit Item</button>"))
-                                            .append($("<button class='btn btn-addItem' data-toggle='modal' data-target='#myCartModal' onclick='setCartID("+json.orders[i].cartid + ", " + json.orders[i].pizzatype + ")' type='button'>Edit Item</button>"))
+                                            .append($("<button class='btn btn-addItem' data-toggle='modal' data-target='#myCartModal' onclick='setCartID("+json.orders[i].cartid + ', ('  + json.orders[i].pizzatype + "))' type='button'>Edit Item</button>"))
                                             .append($("<button class='btn btn-danger' style='margin-left: 3em;' onclick='removeCartItem("+json.orders[i].cartid+")' type='button'>REMOVE</button>")))
                                             
                 
